3aa975ee51e66d831d37d5ce5e5c04b12ff71cf2
Vishwajeet Mandal <krvish21@gmail.com>
2025-05-26 20:28:22 +0530

    user auth

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,11 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
+if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: false // Since we're using serverless functions, we don't need to persist the session
   }
